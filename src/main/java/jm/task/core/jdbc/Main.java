f5a7d73541ccb10e21632298649adab0c850e6ff
Ted Jondon <ted.jondon@yandex.ru>
2021-11-19 19:18:40 +0300

    branch test

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        //test branch
         // реализуйте алгоритм здесь
 
         UserDaoHibernateImpl test = new UserDaoHibernateImpl();
